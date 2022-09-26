c584d669892f9cb86324276e2b6480ffde879481
Sofia2055 <fatimasofiavasquez144@gmail.com>
2022-09-25 18:22:37 -0600

    Initial commit

---
+++ database/migrations/2022_09_21_205048_create_resources_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('description');
+            $table->string('state');
+            $table->integer('resources_quantity');
+            $table->integer('given_quantity');
+            $table->foreignId('center_id');
+            $table->boolean('received');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+};
