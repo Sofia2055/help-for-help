828e41091b1923824a863f1388620a9fc99d3956
Cristia77 <115111792+Cristia77@users.noreply.github.com>
2022-10-05 15:55:18 -0600

    pages

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,74 +4,121 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width, initial-scale=1.0">
-    <style>
-        .inputText{
-    color: black;
-    font-size: large;
-}
-    </style>
-    <link rel="icon" href="img/logo-k.png">
+    <link rel="stylesheet" href="css/form.css">
+    <link rel="icon" href="img/image form.jpg">
     
-    <title>EXPO 2022</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <title>FORM HELP 4 HELP</title>
 </head>
 <body>
+    <div class="container">
 
-    <h1>Formulario Help 4 Help</h1>
+        <div class="row">
+            <div class="col-12">
+                <h1> Form Help 4 Help</h1>
+            </div>
+            <div class="col-4 offset-4">            
+                <form class="resourceForm"> 
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Complete name</label>
+                        <input type="nombrecompleto" class="form-control" id="exampleFormControlInput1" placeholder="Complete name" required>
+                        
+                    </div>
 
-    <form method="post" action="{{ url('/create/resource') }}" enctype="multipart/form-data"> 
-        @csrf
-        <label for="dni" class="inputText">1. Nombre completo  </label><br>
-        <input type="text" id="Nombre completo" placeholder="Pon tu nombre" name="product_name" value="{{ old('product_name') }}"><br>
-        @error('product_name')
-        {{ $message }}<br>
-        @enderror
+                    <div class="mb-4">
+                        <label for="exampleFormControlInput1" class="form-label">E-mail address</label>
+                        <input type="E-mail" class="form-control" id="exampleFormControlInput1" placeholder="Example@gmail.com" required>
+                    </div>
 
-        <label for="dni" class="inputText">2. Direccion de correo electronico</label><br>
-        <input type="text" id="E-mail" placeholder="Pon tu correo electronico"><br>
-        <label>3. Descripción del recurso </label><br>
-        <input type="text" name="description"><br>
-        @error('description')
-        {{ $message }}<br>
-        @enderror
-        <label>4. ¿Cual es el estado del recurso</label><br>
-        <input type="radio" name="state" value="Nuevo">Nuevo<br>
-        <input type="radio" name="state">Usado como nuevo<br>
-        <input type="radio" name="state">Usado<br>
-        @error('state')
-        {{ $message }}<br>
-        @enderror
-        <label>5. Cantidad</label><br>
-        <input type="number" name="resources_quantity"><br>
-        @error('resources_quantity')
-        {{ $message }}<br>
-        @enderror
-        <label>6. Adjunte fotos del recurso </label><br>
-        <input type="file" name="photos"><br>
-        <label>7.Elija su Centro Supérate </label><br>
-        <input type="radio" value="1" name="center_id">Centro ¡Supérate! Merlet<br>
-        <input type="radio">Centro ¡Supérate! Hilasal<br>
-        <input type="radio">Centro ¡Supérate! Grupo Q<br>
-        <input type="radio">Centro ¡Supérate! ADOC<br>
-        <input type="radio">Centro ¡Supérate! Raíces<br>
-        <input type="radio">Centro ¡Supérate! Fundación Poma<br>
-        <input type="radio">Autorizo al Centro !Superate! a utilizar la informacion anteriormente brindada para ser publicada en la pagina de "Help For Help".<br>
-        @error('center_id')
-        {{ $message }}<br>
-        @enderror
-        <a href="01.html">« Volver</a>
+                    <div class="mb-5">
+                        <label for="exampleFormControlInput1" class="form-label">Resource description</label>
+                        <input type="resource description" class="form-control" id="exampleFormControlInput1" placeholder="resource description" required>
+                    </div>
 
-        @if($errors->any())
-    {!! implode('', $errors->all('<div>:message</div>')) !!}
-@endif
+                    <div class="mb-6">
+                        <label for="exampleFormControlInput1" class="form-label">Quantity</label>
+                        <input type="resource description" class="form-control" id="exampleFormControlInput1" placeholder="Quantity" required>
+                    </div>
+
+                    <div class="mb-7">
+                        <label for="exampleFormControlInput1" class="form-label">Resource status</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                        <label class="form-check-label" for="flexRadioDefault1"></label>
+                        
+                        New
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                        <label class="form-check-label" for="flexRadioDefault1"></label>
+                        Used as new
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                        <label class="form-check-label" for="flexRadioDefault1"></label>
+                        Used 
+                    </div>
+
+                    <div class="input-group mb-8">
+
+                     <div class="mb-8">
+                        <label for="exampleFormControlInput1" class="form-label">Upload product photos</label>
+                    <input type="file" name="photos" class="inputForm"><br>
+                    </div>
+
+                    <label for="exampleFormControlInput1" class="form-label">Choose your center Superate</label>
+                    <div class="input-group mb-8">
+                    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Open this select menu</option>
+                        <option value="1">Centro ¡Supérate! Merlet
+                        </option>
+                        <option value="2">Centro ¡Supérate! Hilasal</option>
+                        <option value="3">Centro ¡Supérate! Grupo Q</option>
+                        <option value="4">Centro ¡Supérate! ADOC</option>
+                        <option value="5">Centro ¡Supérate! Raíces</option>
+                        <option value="6">Centro ¡Supérate! Fundación Poma</option>
 
 
+                      </select>
 
+                   </div>
 
+                   <div class="col-12">
+                    <button class="btn btn-primary" type="submit">Send form</button>
+                  </div>
 
-        <input type="submit">
+                  
 
+                    
+                  
+                   
+                   
+                  
+                   <a href="01.html">« Volver</a>
+                 
 
-    </form>
-
+                    
+            
+            
+            
+            
+            
+            
+            
+                
+            
+            
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>
