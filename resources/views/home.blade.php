65d0292d831f7872ba76b13d8dc686d4d15ae6cc
Cristia77 <115111792+Cristia77@users.noreply.github.com>
2022-10-05 14:40:48 -0600

    home blade + css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@500;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/01.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="icon" href="img/logo-k.png">
+    <title>Help for Help</title>
+</head>
+<body>
+<section id="header">
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="9" class="active"></li>     
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="img/slider_template_0005s_0001_Lineas.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/fondo superate.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="img/imagen carrusel 1.jpg" alt="Third slide">
+          </div>+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      
+      </div>
+      
+    <header class="OLA">
+      <div class="fixed-bottom">...
+        <nav class="nav">
+        <div class="navegacion">
+            <img src="img/logo-k.png" alt="">
+            <h1 class="help"><h>Help for help</h></h1>
+            <ul class="nav-menu nav-menu_visible">
+              
+              <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle"
+                        data-toggle="dropdown">
+                  Login<span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="02.html">login Admin</a></li>
+                  
+              </div>
+                <a href="" class="nav-menu-link nav-link Tutorial"><b>Tutorial</b></a></li>
+                <a href="FORM.HTML" class="nav-menu-link nav-link tres"><b>Giving back</b></a></li>
+                <a href="" class="nav-menu-link nav-link dos"><b>Resources</b></a></li>
+
+                <a href="" class="nav-menu-link nav-link uno"><b>Home</b></a></li>
+                <div class="user">
+               </div>
+            </ul> 
+        </div>
+        </nav>
+    </header>
+    <img src="img/logo-k.png" alt="muestra">
+    <div class="tarjeta">
+      <div class="titulo">Get to know our "Help For Help" project</div>
+      <div class="cuerpo">
+      
+
+      This project will help meet the needs that need to arise in the families of Superate students, 
+      and provide them with a way to apply for help. We will support the strength 
+      of more people on the outside, who have the opportunity to give, and who also want to lend a hand.
+      </div>
+      
+      </div>
+    </div>
+  
+    
+
+
+</section>
+
+
+    
+    
+    <footer class="text-center text-lg-start bg-light text-muted">
+      <!-- Section: Social media -->
+      <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+          <span>Our information</span>
+        </div>
+        <!-- Left -->
+    
+        <!-- Right -->
+
+        <div>
+          
+          <button>
+            <a href="https://es-la.facebook.com/superate.poma.5/" class="me-4 text-reset">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            </a>
+            <a href="https://www.superate.org.sv/centro-superate-fundacion-poma/" class="me-4 text-reset">
+              <i class="fab fa-google"></i>
+            </a>
+            <a href="https://instagram.com/superatepoma?igshid=YmMyMTA2M2Y=" class="me-4 text-reset">
+              <i class="fab fa-instagram"></i>
+            </a>
+         </button>
+          </a>
+        </div>
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+    
+      <!-- Section: Links  -->
+      <section class="footer">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                <i class="fas fa-gem me-3"></i>Help for Help
+              </h6>
+              <p>
+                
+              </p>
+            </div>
+            <!-- Grid column -->
+    
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                Options
+              </h6>
+              <p>
+                <a href="#!" class="text-reset">RESOURCES</a>
+              </p>
+              <p>
+          
+              <p>
+                <a href="FORM.HTML" class="text-reset">GIVING BACK</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+    
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                Useful links
+              </h6>
+              <p>
+                <a href="https://www.superate.org.sv/centro-superate-fundacion-poma/" class="text-reset">Superate centers</a>
+              </p>
+              <p></p>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Tutorial</a>
+              </p>
+             
+            </div>
+            <!-- Grid column -->
+    
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <p><i class="fas fa-home me-3"></i> Centro Superate Fundacion Poma</p>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                info@poma.superate.org.sv
+              </p>
+              <p><i class="fas fa-phone me-3"></i>+503 2229-7239</p>
+             
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+    
+      <!-- Copyright -->
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2022 Help for Help:
+      
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+
+</section>
+<script src="https://kit.fontawesome.com/a4f39d8085.js" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
