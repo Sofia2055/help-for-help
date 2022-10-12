dd88529f862fa3b221f50ded2aca418aab2e0fb5
Franrauda <115113878+Franrauda@users.noreply.github.com>
2022-10-12 15:44:51 -0500

    login finished

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,25 +5,26 @@
                 <div class="col-12">
                     <div class="card my-3">
                         <div class="row g-0 my-5">
-                            <div class="col-md-4 order-2 order-md-2">
+                            <div class="col-md-4 order-3 order-md-2">
                                 <div class="col-10 offset-1">
                                     <form action="{{ url('login') }}" method="POST">
                                         @csrf
                                         <x-input type="email" label="Email address" name="email" :required="true"/>
                                         <x-input type="password" label="Password" name="password" :required="true"/>
-                                        <button type="submit" class="btn btn-primary w-100">Login</button>
+                                        <button type="submit" class="btn btn-primary w-100 color-aqua">Login</button>
                                     </form>
                                 </div>
                             </div>
-                            <div class="col-md-8 order-1 order-md-2">
+                            <div class="col-md-6 order-2 order-md-2">
                                 <div class="col-10 offset-1">
                                     <div class="card-body">
                                         <h1 class="card-title text-center">Help for help<br>Administration</h1>
-                                        <p class="card-text">This is a wider card with supporting text below as a
-                                            natural
-                                            lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text text-center">Type the email address you were assigned to log in to administration.</p>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="col-md-2 col-8 offset-2 offset-md-0 order-1 order-md-2 pe-4 d-flex flex-wrap align-items-center">
+                                <img src="{{ url('img/logo-k.png') }}" width="100%">
                             </div>
                         </div>
                     </div>
