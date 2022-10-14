3969502e2af3ff3aee4c01ad5849bad0bd064796
Sofia2055 <fatimasofiavasquez144@gmail.com>
2022-10-14 14:55:06 -0600

    Resources images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,10 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(5)->create([
             'center_id' => 2
         ]);
+
 // ¡Superate!'s centers
+
+       // center_id = 1
         \App\Models\Center::factory()->create([
             'name' => '¡Supérate! Fundación Poma',
             'description' => 'Inagurado en el año 2011 gracias a la Fundación Poma, al convenio firmado
@@ -34,6 +37,7 @@ class DatabaseSeeder extends Seeder
             'address' => '4 Calle Pte. entre 10av y, 8 Avenida Sur 5-4, Santa Tecla'
         ]);
 
+        // center_id = 2
         \App\Models\Center::factory()->create([
             'name' => '¡Supérate! ADOC',
             'description' => 'Se inauguró en el año 2008 gracias a la iniciativa de la Fundación ADOC
@@ -42,6 +46,7 @@ class DatabaseSeeder extends Seeder
             'address' => 'MRWW+782, CA-1, San Salvador'
         ]);
 
+        // center_id = 3
         \App\Models\Center::factory()->create([
             'name' => '¡Supérate! Fundación Raíces',
             'description' => 'Grupo Scout 12 "Los Conacastecos
@@ -51,6 +56,7 @@ class DatabaseSeeder extends Seeder
             'address' => ' PV95+3Q9, Soyapango'
         ]);
 
+        // center_id = 4
         \App\Models\Center::factory()->create([
             'name' => '¡Supérate! Merlet',
             'description' => 'Inagurado en el 2011 gracias a la Fundación Frech y USAID en el 2010
@@ -59,6 +65,7 @@ class DatabaseSeeder extends Seeder
             'address' => 'Calle Circunvalación, Poligono D #17, Plan de La Laguna'
         ]);
 
+        // center_id = 5
         \App\Models\Center::factory()->create([
             'name' => '¡Supérate! Hilasal',
             'description' => 'Fue una iniciativa de la Fundacion Sagrera Polomo. Se inaguró en el año
@@ -68,6 +75,7 @@ class DatabaseSeeder extends Seeder
             'address' => ' QJW6+896, Sitio del Niño'
         ]);
 
+        // center_id = 6
         \App\Models\Center::factory()->create([
             'name' => '¡Supérate! Grupo Q ',
             'description' => 'Se unio a ¡Supérate! en el año 2018 como patrocinador del Centro en San Miguel,
@@ -76,18 +84,228 @@ class DatabaseSeeder extends Seeder
             'address' => '9A Calle Oriente, San Miguel'
         ]);
 
-
-        \App\Models\Resource::factory(10)->create();
-
-        //Resources 1
+        //Resource 1
         \App\Models\Resource::factory()->create([
             'product_name' => "Sacapuntas",
             'center_id' => 1,
-            'description' => 'Sacapuntas que me sobraron',
-            'resources_quantity' => 7,
+            'description' => 'Sacapuntas de metal',
+            'resources_quantity' => 4,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => true
+        ]);
+
+        //Resource 2
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Colores New Elite ",
+            'center_id' => 1,
+            'description' => 'Contiene 12 colores punta gruesa ',
+            'resources_quantity' => 2,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => true
+        ]);
+
+        //Resource 3
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Juego de reglas ",
+            'center_id' => 1,
+            'description' => 'contiene 4 diferentes reglas',
+            'resources_quantity' => 2,
             'given_quantity' => 0,
             'state' => 'as new',
             'received' => true
+        ]);
+
+        //Resource 4
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Crayolas Facela",
+            'center_id' => 1,
+            'description' => 'Contiene 12 crayolas de cera',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 5
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Borrador Parrot",
+            'center_id' => 1,
+            'description' => 'color blanco y suave',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => false
+        ]);
+
+        //Resource 6
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Borrador Facela",
+            'center_id' => 1,
+            'description' => 'Borrador suave y blanco',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => true
+        ]);
+
+        //Resource 7
+        //Falta fotografia
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Colores Surtidos",
+            'center_id' => 1,
+            'description' => 'Colores doble punta, 44 colores de diferentes tonos, bicolor',
+            'resources_quantity' => 44,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 8
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Colores Facela ",
+            'center_id' => 1,
+            'description' => 'Trae 12 colores cada caja',
+            'resources_quantity' => 2,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => false
+        ]);
+
+        //Resource 9
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Estuche ",
+            'center_id' => 1,
+            'description' => 'Estuche para lapices de metal',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'as new',
+            'received' => true
+        ]);
+
+        //Resource 10
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Tijera",
+            'center_id' => 1,
+            'description' => 'Tijera color azul de metal',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => true
+        ]);
+
+        //Resource 11
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Lapiceros",
+            'center_id' => 1,
+            'description' => 'colores disponibles: rojo, negro y azul surtidos',
+            'resources_quantity' => 6,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 12
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Caja de colores Falcon",
+            'center_id' => 1,
+            'description' => ' 12 colores x caja y bicolores',
+            'resources_quantity' => 3,
+            'given_quantity' => 0,
+            'state' => 'as new',
+            'received' => true
+        ]);
+
+        //Resource 13
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Crayolas",
+            'center_id' => 1,
+            'description' => 'Colores surtidos',
+            'resources_quantity' => 5,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => false
+        ]);
+
+        //Resource 14
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Regla",
+            'center_id' => 1,
+            'description' => 'Regla de metal color azul 30 cm',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'as new',
+            'received' => true
+        ]);
+
+        //Resource 15
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Plumones",
+            'center_id' => 1,
+            'description' => '17 diferentes plumones',
+            'resources_quantity' => 17,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 16
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Cuadernos",
+            'center_id' => 1,
+            'description' => 'Rayados y cuadriculados con espiral',
+            'resources_quantity' => 57,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 17
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Libreta papel bond ",
+            'center_id' => 1,
+            'description' => 'paginas blancas tamaño oficio de 100 paginas',
+            'resources_quantity' => 1,
+            'given_quantity' => 0,
+            'state' => 'new',
+            'received' => true
+        ]);
+
+        //Resource 18
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Paginas de papel bond ralladas",
+            'center_id' => 1,
+            'description' => '150 paginas de papel bond ralladas',
+            'resources_quantity' => 150,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 19
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Libros Oxford",
+            'center_id' => 1,
+            'description' => 'Libros "reading and writing" Oxford',
+            'resources_quantity' => 5,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => true
+        ]);
+
+        //Resource 20
+        //Falta Fotografias
+        \App\Models\Resource::factory()->create([
+            'product_name' => "Libros servicios educativos(lenguaje,sociales,
+            moral urbanidad y civica, y ciencias)",
+            'center_id' => 1,
+            'description' => 'Libros de primer año bachillerato de lenguaje,sociales,
+            moral urbanidad y civica, y ciencias',
+            'resources_quantity' => 4,
+            'given_quantity' => 0,
+            'state' => 'used',
+            'received' => false
         ]);
     }
 }
