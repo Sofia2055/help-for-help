988cd0b9e02fc5f37f8814ab8627904d003250d7
Cristia77 <115111792+Cristia77@users.noreply.github.com>
2022-10-21 21:00:11 -0600

    Tutorial

--- resources/views/tutorial.blade.php
+++ resources/views/tutorial.blade.php
@@ -7,12 +7,12 @@
         <!-- Esto es una image con su texto -->
         <div class="row">
             <div class="col-12 col-md-8 offset-md-2">
-                <img src="{{ asset('tutorial/example .png') }}"width="100%">
+                <img src="{{ asset('tutorial/home.png') }}"width="100%">
             </div>
         </div>
         <div class="row">
             <div class="col-12 text-center">
-                Hacer clik en le boton de Giving back
+                Hacer click en el boton de Giving back
 
 
 
@@ -22,25 +22,49 @@
 
         <div class="row">
             <div class="col-12 col-md-8 offset-md-2">
-                <img src="{{ asset('tutorial/example .png') }}"width="100%">
+                <img src="{{ asset('tutorial/Form.png') }}"width="100%">
             </div>
         </div>
         <div class="row">
             <div class="col-12 text-center">
-                Hacer clik en le boton de Giving back
+               Llena el formulario con los datos correctos
             </div>
         </div>
 
         <div class="row">
             <div class="col-12 col-md-8 offset-md-2">
-                <img src="{{ asset('tutorial/example .png') }}"width="100%">
+                <img src="{{ asset('tutorial/elegir estado.png') }}"width="100%">
             </div>
         </div>
         <div class="row">
             <div class="col-12 text-center">
-                Hacer clik en le boton de Giving back
+                Elige el estado del producto
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-12 col-md-8 offset-md-2">
+                <img src="{{ asset('tutorial/ELEGIR CENTRO SUPERATE.png') }}"width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                Elige el Centro Superate al que entregaras el producto y apretar el boton enviar formulario.
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 col-md-8 offset-md-2">
+                <img src="{{ asset('tutorial/HECHO.png') }}"width="100%">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                Cuando envies el formulario te saldra una alerta en el home y sera que tu solicitud fue recibida.
+            </div>
+        </div>
+
+
 
     </div>
 </x-body>
