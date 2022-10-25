3cf5ea2e75565f35eb9e2071e9b807a8cc0934ae
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2022-10-25 13:13:02 -0600

    offset fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,7 +49,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-10 offset-1">
+        <div class="col-12 col-md-10 offset-md-1">
             <div class="row row-cols-1 row-cols-sm-2 2row-cols-md-3 row-cols-lg-3 g-3">
                 @foreach ($centers as $center)
                     <div class="col">
